d7a020af5ec1b96f149c7eafd99574dd792275d2
Long Nguyen The Phung <thelonglqd@gmail.com>
2018-02-13 20:30:51 +0700

    add model methods and controller API for users

--- models/users.js
+++ models/users.js
@@ -1,3 +1,7 @@
+import bcrypt from 'bcrypt';
+
+const saltRounds = 10;
+
 export default (sequelize, DataTypes) => {
   const User = sequelize.define('User', {
     username: { type: DataTypes.STRING, allowNull: false },
@@ -7,9 +11,33 @@ export default (sequelize, DataTypes) => {
     timestamps: true,
   });
 
+  // ASSOCIATION
   User.associate = (models) => {
     User.hasMany(models.Post);
   };
+
+  User.prototype.toJSON = function toJSON() {
+    const values = Object.assign({}, this.get());
+    delete values.password;
+    return values;
+  };
+
+  // HOOKS
+  /* eslint-disable no-param-reassign */
+  User.hook('beforeCreate', user => bcrypt.hash(user.password, saltRounds)
+    .then((hashed) => {
+      user.password = hashed;
+    }));
+
+  // METHOD
+  User.createUser = async user => User.create(user);
+  User.readUser = async id => User.findById(id);
+  User.updateUser = async (instance, changes) => instance.update(changes);
+  User.listUsers = async (offset = 0, limit = 20) => ({
+    offset,
+    limit,
+    data: await User.findAll(),
+  });
 
   return User;
 };
--- routes/users.js
+++ routes/users.js
@@ -1,12 +1,61 @@
 import express from 'express';
+
 import { User } from '../models';
+import log from '../logger/';
 
 const router = express.Router();
 
 /* GET users listing. */
-router.get('/', (req, res) => {
-  User.findAll({ include: ['Posts', 'Groups'] })
-    .then(users => res.send(users));
+router.get('/', async (req, res, next) => {
+  try {
+    const { offset, limit } = req.query;
+    const users = await User.listUsers(offset, limit);
+    res.json(users);
+  } catch (error) {
+    log.error(error);
+    next(error);
+  }
+});
+
+/* GET user by id */
+router.get('/:userId', async (req, res) => {
+  try {
+    const { userId } = req.params;
+    const user = await User.readUser(userId);
+    res.json(user);
+  } catch (error) {
+    log.error(error);
+    res.json(error);
+  }
+});
+
+/* POST create a user */
+router.post('/', async (req, res) => {
+  try {
+    const user = req.body;
+    log.debug('user: ', user);
+    const createdUser = await User.createUser(user);
+    res.json(createdUser);
+  } catch (error) {
+    log.error(error);
+    res.json(error);
+  }
+});
+
+/* PATH update a user */
+router.patch('/:userId', async (req, res) => {
+  try {
+    const { userId } = req.params;
+    const changes = req.body;
+    const updating = await User.readUser(userId);
+    const updated = await User.updateUser(updating, changes);
+    delete updated.password;
+    log.debug('updated user: ', updated);
+    res.json(updated);
+  } catch (error) {
+    log.error(error);
+    res.json(error);
+  }
 });
 
 module.exports = router;
